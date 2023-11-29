c779a1aa2b4d53c2c5159794e940f8ecfb3768b8
Hussein Sadiq Elged <113722584+elged194@users.noreply.github.com>
2023-11-29 18:58:33 +0200

    Update active.js

--- js/active.js
+++ js/active.js
@@ -35,151 +35,6 @@
     });
 
     /*=======================
-		  Slider Range JS
-		=========================*/
-    $(function () {
-      $("#slider-range").slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [120, 250],
-        slide: function (event, ui) {
-          $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-        },
-      });
-      $("#amount").val(
-        "$" +
-          $("#slider-range").slider("values", 0) +
-          " - $" +
-          $("#slider-range").slider("values", 1)
-      );
-    });
-
-    /*=======================
-		  Home Slider JS
-		=========================*/
-    $(".home-slider").owlCarousel({
-      items: 1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      smartSpeed: 400,
-      animateIn: "fadeIn",
-      animateOut: "fadeOut",
-      autoplayHoverPause: true,
-      loop: true,
-      nav: true,
-      merge: true,
-      dots: false,
-      navText: [
-        '<i class="ti-angle-left"></i>',
-        '<i class="ti-angle-right"></i>',
-      ],
-      responsive: {
-        0: {
-          items: 1,
-        },
-        300: {
-          items: 1,
-        },
-        480: {
-          items: 1,
-        },
-        768: {
-          items: 3,
-        },
-        1170: {
-          items: 4,
-        },
-      },
-    });
-
-    /*===========================
-		  Quick View Slider JS
-		=============================*/
-    $(".quickview-slider-active").owlCarousel({
-      items: 1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      smartSpeed: 400,
-      autoplayHoverPause: true,
-      nav: true,
-      loop: true,
-      merge: true,
-      dots: false,
-      navText: [
-        '<i class=" ti-arrow-left"></i>',
-        '<i class=" ti-arrow-right"></i>',
-      ],
-    });
-
-    /*===========================
-		  Home Slider 4 JS
-		=============================*/
-    $(".home-slider-4").owlCarousel({
-      items: 1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      smartSpeed: 400,
-      autoplayHoverPause: true,
-      nav: true,
-      loop: true,
-      merge: true,
-      dots: false,
-      navText: [
-        '<i class=" ti-arrow-left"></i>',
-        '<i class=" ti-arrow-right"></i>',
-      ],
-    });
-
-    /*====================================
-		14. CountDown
-		======================================*/
-    $("[data-countdown]").each(function () {
-      var $this = $(this),
-        finalDate = $(this).data("countdown");
-      $this.countdown(finalDate, function (event) {
-        $this.html(
-          event.strftime(
-            '<div class="cdown"><span class="days"><strong>%-D</strong><p>Days.</p></span></div><div class="cdown"><span class="hour"><strong> %-H</strong><p>Hours.</p></span></div> <div class="cdown"><span class="minutes"><strong>%M</strong> <p>MINUTES.</p></span></div><div class="cdown"><span class="second"><strong> %S</strong><p>SECONDS.</p></span></div>'
-          )
-        );
-      });
-    });
-
-    /*====================================
-		16. Flex Slider JS
-		======================================*/
-    (function ($) {
-      "use strict";
-      $(".flexslider-thumbnails").flexslider({
-        animation: "slide",
-        controlNav: "thumbnails",
-      });
-    })(jQuery);
-
-    /*====================================
-		  Cart Plus Minus Button
-		======================================*/
-    var CartPlusMinus = $(".cart-plus-minus");
-    CartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
-    CartPlusMinus.append('<div class="inc qtybutton">+</div>');
-    $(".qtybutton").on("click", function () {
-      var $button = $(this);
-      var oldValue = $button.parent().find("input").val();
-      if ($button.text() === "+") {
-        var newVal = parseFloat(oldValue) + 1;
-      } else {
-        // Don't allow decrementing below zero
-        if (oldValue > 0) {
-          var newVal = parseFloat(oldValue) - 1;
-        } else {
-          newVal = 1;
-        }
-      }
-      $button.parent().find("input").val(newVal);
-    });
-
-    /*=======================
 		  Extra Scroll JS
 		=========================*/
     $(".scroll").on("click", function (e) {
@@ -204,33 +59,6 @@
       } else {
         $(this).parent("label").removeClass("checked");
       }
-    });
-
-    /*==================================
-		 12. Product page Quantity Counter
-		 ===================================*/
-    $(".qty-box .quantity-right-plus").on("click", function () {
-      var $qty = $(".qty-box .input-number");
-      var currentVal = parseInt($qty.val(), 10);
-      if (!isNaN(currentVal)) {
-        $qty.val(currentVal + 1);
-      }
-    });
-    $(".qty-box .quantity-left-minus").on("click", function () {
-      var $qty = $(".qty-box .input-number");
-      var currentVal = parseInt($qty.val(), 10);
-      if (!isNaN(currentVal) && currentVal > 1) {
-        $qty.val(currentVal - 1);
-      }
-    });
-
-    /*=====================================
-		15.  Video Popup JS
-		======================================*/
-    $(".video-popup").magnificPopup({
-      type: "iframe",
-      removalDelay: 300,
-      mainClass: "mfp-fade",
     });
 
     /*====================================
@@ -278,17 +106,42 @@
   setTimeout(function () {
     //After 2s, the no-scroll class of the body will be removed
     $("body").removeClass("no-scroll");
-  }, 500); //Here you can change preloader time
+  }, 1000); //Here you can change preloader time
 })(jQuery);
 
 /*=====================================
 	  jq-slider flipster
+    index
 	======================================*/
-
 $(function () {
   var coverflow = $("#coverflow").flipster();
 });
-
+window.jQuery ||
+  document.write('<script src="/js/jquery-1.10.2.min.js"></script>');
+/*=====================================
+   Item-name	  
+   product-name
+	======================================*/
+var owl = $(".product-name .owl-carousel");
+owl.owlCarousel({
+  loop: true,
+  nav: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    960: {
+      items: 3,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
 /*=====================================
    index	  
    Women Banner & Reviews
@@ -303,7 +156,7 @@ owl.owlCarousel({
       items: 1,
     },
     600: {
-      items: 2,
+      items: 3,
     },
     960: {
       items: 3,
@@ -313,20 +166,11 @@ owl.owlCarousel({
     },
   },
 });
-owl.on("mousewheel", ".owl-stage", function (e) {
-  if (e.deltaY > 0) {
-    owl.trigger("next.owl");
-  } else {
-    owl.trigger("prev.owl");
-  }
-  e.preventDefault();
-});
+
 /*=====================================
-	  img item-name
+    // page item-name
+    // product-name
 	======================================*/
-
-// ***************//
-
 let imgs = document.querySelectorAll(".item-img img");
 let slider = document.querySelector(".figure img");
 imgs.forEach((img) => {
@@ -334,11 +178,10 @@ imgs.forEach((img) => {
     slider.src = e.target.src;
   });
 });
-
 /*=====================================
 	  //cart page
-     //Shopping Summery 
-     //Remove a table from the list within the cart page in Shopping Summery 
+    //Shopping Summery 
+    //Remove a table from the list within the cart page in Shopping Summery 
 	======================================*/
 
 const close = document.querySelectorAll("#remove-icon");
@@ -363,23 +206,9 @@ function openCity(cityName) {
 }
 
 /*=====================================
-	  //MyAccount page
-    //class name Account
-	======================================*/
-// function openCity(Reviews) {
-//   var i;
-//   var x = document.getElementsByClassName("Yuor-Reviews");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   document.getElementById(Reviews).style.display = "block";
-// }
-
-/*=====================================
 	  //page index
     //class name deal-of-week
 	======================================*/
-
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2025 15:37:25").getTime();
 
@@ -397,22 +226,24 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
-  // document.getElementById("demo").innerHTML = `
-  //  ${days } :  ${hours}  :  ${minutes } :  ${seconds}`;
-  document.getElementById("d").innerText = days;
-  document.getElementById("h").innerText = hours;
-  document.getElementById("m").innerText = minutes;
-  document.getElementById("s").innerText = seconds;
+  // Update the HTML elements
+  document.getElementById("days").innerHTML = days + "<span>Days</span>";
+  document.getElementById("hours").innerHTML = hours + "<span>Hours</span>";
+  document.getElementById("minutes").innerHTML =
+    minutes + "<span>Minutes</span>";
+  document.getElementById("seconds").innerHTML =
+    seconds + "<span>Seconds</span>";
 
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("d", "h", "m", "s").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "<div>EXPIRED</div>";
   }
 }, 1000);
+/*=====================================
+    End deal-of-week
+======================================*/
 
-// ************************
 function quantityChanged(event) {
   var input = event.target;
   if (isNaN(input.value) || input.value <= 0) {
@@ -420,3 +251,8 @@ function quantityChanged(event) {
   }
   updatetotel();
 }
+// WOW js
+document.addEventListener("DOMContentLoaded", function () {
+  new WOW().init();
+});
+//
